c21be737d69c85f31f9a0757e333b3ea53ff7fba
louisld <louisld@users.noreply.github.com>
2016-10-31 09:48:36 +0100

    Change scale of axises

--- src/main/java/fr/bloome/tpegraph/Main.java
+++ src/main/java/fr/bloome/tpegraph/Main.java
@@ -1,5 +1,6 @@
 package fr.bloome.tpegraph;
 
+import java.awt.Color;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,14 +8,18 @@ import java.util.List;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\louis\\Documents\\Ambicitil_1d8cb0f9_2016-10-24-21-56-45.csv");
+		File file = new File("/home/louisld/workspace/TpeGraph/Ambicitil_1d8cb0f9_2016-10-24-21-56-45.csv");
 		CvsFileReader reader = new CvsFileReader(file);
 		
 		//Temp Arrays
@@ -44,7 +49,7 @@ public class Main {
 		
 		//Fill the arrays
 		for(String str : col2){
-			timeFromStart.add(Double.parseDouble(str.replaceAll("\"", "")) - minTime);
+			timeFromStart.add((Double.parseDouble(str.replaceAll("\"", "")) - minTime) / 60000);
 		}
 		for(String str : col5){
 			decibel.add(Float.parseFloat(str.replaceAll("\"", "")));
@@ -57,15 +62,27 @@ public class Main {
 		collection.addSeries(ligne6);
 		
 		JFreeChart chart = ChartFactory.createXYLineChart(
-				"Niveau sonore des lignes de métro",      // chart title
-				"Seconde",                      // x axis label
-				"Décibel",                      // y axis label
+				"Niveau sonore des lignes de mÃ©tro",      // chart title
+				"Minutes",                      // x axis label
+				"DÃ©cibel",                      // y axis label
 				collection,                  // data
 				PlotOrientation.VERTICAL,
 				true,                     // include legend
 				true,                     // tooltips
 				false                     // urls
 				);
+		 XYPlot xyPlot = (XYPlot) chart.getPlot();
+        xyPlot.setDomainCrosshairVisible(true);
+        xyPlot.setRangeCrosshairVisible(true);
+        XYItemRenderer renderer = xyPlot.getRenderer();
+        renderer.setSeriesPaint(0, Color.blue);
+        NumberAxis domain = (NumberAxis) xyPlot.getDomainAxis();
+        domain.setRange(0.00, 35.00);
+        domain.setTickUnit(new NumberTickUnit(10.0));
+        domain.setVerticalTickLabels(true);
+        NumberAxis range = (NumberAxis) xyPlot.getRangeAxis();
+        range.setRange(40.0, 90.0);
+        range.setTickUnit(new NumberTickUnit(5.0));
 		MainWindow window = new MainWindow(chart);
 	}
 
